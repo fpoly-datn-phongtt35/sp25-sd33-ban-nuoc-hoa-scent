f5bf7aaedcf775c6131864427ec70b16c6bf58bf
kien200413 <kienttph42696@fpt.edu.vn>
2025-03-02 18:51:37 +0700

    phie giam gia admin

--- src/app/admin/vourcher/vourcher.component.ts
+++ src/app/admin/vourcher/vourcher.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { PhieugiamgiaService } from '../../service/phieugiamgia.service';
+import { CommonModule } from '@angular/common';
 
+import { Injectable, Inject } from '@angular/core';
 @Component({
   selector: 'app-vourcher',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vourcher.component.html',
   styleUrl: './vourcher.component.scss'
 })
 export class VourcherComponent {
+  phieuGiamGias: any[] = [];
 
+  constructor(private phieuGiamGiaService: PhieugiamgiaService) { }
+
+  ngOnInit(): void {
+    this.loadAllPhieuGiamGia();
+  }
+
+  loadAllPhieuGiamGia(): void {
+    this.phieuGiamGiaService.getAllPhieuGiamGia().subscribe(
+      (      data: any[]) => {
+        this.phieuGiamGias = data;
+        console.log('Phiếu giảm giá:', data);
+      },
+      (      error: any) => {
+        console.error('Có lỗi xảy ra khi lấy dữ liệu phiếu giảm giá:', error);
+      }
+    );
+  }
 }
---
+++ src/app/service/phieugiamgia.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable,throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { TokenService } from './token.service';
+
+@Injectable({
+  providedIn: 'root',  // Đảm bảo rằng service được cung cấp ở cấp độ root
+})
+
+export class PhieugiamgiaService {
+    constructor(private http: HttpClient) { }
+  private apiUrl = 'http://localhost:8080/rest/phieu-giam-gia/getAll'; // URL API của bạn
+
+  getAllPhieuGiamGia(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+}
