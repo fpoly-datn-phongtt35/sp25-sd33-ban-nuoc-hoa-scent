3829104c8728886e6685d010d0a1b0cd7aea8246
kien200413 <kienttph42696@fpt.edu.vn>
2025-04-02 02:11:02 +0700

    xem chi tiet don hang theo usrID

--- src/app/order-detail-user-id/order-detail-user-id.component.ts
+++ src/app/order-detail-user-id/order-detail-user-id.component.ts
@@ -3,18 +3,20 @@ import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../service/user.service';
 import { TokenService } from '../service/token.service';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-order-detail-user-id',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,HeaderComponent, FooterComponent],
   templateUrl: './order-detail-user-id.component.html',
   styleUrl: './order-detail-user-id.component.scss'
 })
 export class OrderDetailUserIDComponent implements OnInit{
   orders: any[] = [];
   userId: number=0;
-
+  selectedOrder: any = null;
   constructor(private userService: UserService,private tokenService: TokenService) {
    
   }
@@ -42,12 +44,34 @@ export class OrderDetailUserIDComponent implements OnInit{
   }
   getStatusLabel(statusCode: number): string {
     switch (statusCode) {
-      case 1: return 'Chờ xác nhận';
-      case 2: return 'Đã xác nhận';
-      case 3: return 'Đang giao';
-      case 4: return 'Đã thanh toán';
-      case 5: return 'Đã hủy';
-      default: return 'Trạng thái không xác định';
+      case 1: return 'Chờ xác nhận';  // Trạng thái chờ xác nhận
+      case 2: return 'Đã xác nhận';  // Trạng thái đã xác nhận
+      case 3: return 'Đang giao';  // Trạng thái đang giao
+      case 4: return 'Hoàn thành';  // Trạng thái đã hoàn thành
+      case 5: return 'Đã hủy';  // Trạng thái đã hủy
+      case 6: return 'Đã thanh toán';  // Trạng thái đã thanh toán (chuyển khoản)
+      default: return 'Trạng thái không xác định';  // Nếu không phải một trong các trạng thái trên
     }
   }
+  
+  openModal(order: any) {
+    console.log("Đơn hàng đã được nhấn:", order);
+    this.selectedOrder = order;
+    console.log("selectedOrder:", this.selectedOrder);
+  }
+
+  closeModal() {
+    this.selectedOrder = null;
+  }
+   toggleOrderDetail(order: any) {
+    if (this.selectedOrder === order) {
+      this.selectedOrder = null; // Nếu đã chọn đơn hàng thì bỏ chọn
+    } else {
+      this.selectedOrder = order; // Chọn đơn hàng mới
+    }
+  }
+  goBack() {
+    this.selectedOrder = null; // Quay lại danh sách đơn hàng
+  }
+  
 }
